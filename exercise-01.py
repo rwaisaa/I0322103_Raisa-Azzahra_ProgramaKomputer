70e3dfd2d34d0bc4e67d44a48570064ba7297d06
rwaisaa <azzahraraisa42@student.uns.ac.id>
2023-03-13 17:24:32 +0700

    Menambahkan file exercise-01

---
+++ exercise-01.py
@@ -0,0 +1,11 @@
+nama = "Raisa Azzahra"
+NIM = "I0322103"
+kampungHalaman = "Tanjungpinang"
+Visualisasi = "Tanjungpinang adalah kota kecil di pulau Sumatera yang terletak di dekat kota Batam dan berseberangan dengan Negara Singapura dan Malaysia. Tanjungpinang dikelilingi oleh lautan sehingga banyak pantai. Oleh karena itu, ikan tidak sulit dicari dan nelayan menjadi mata pencaharian utama penduduk"
+harapan = "Sebagai lulusan teknik industri saya ingin bekerja di suatu pabrik atau industri manufaktur terbaik di Indonesia contohnya seperti PT. Astra Internasional Tbk, PT. Gudang Garam Tbk, dan PT Semen Indonesia"
+
+print(nama)
+print(NIM)
+print(kampungHalaman)
+print(Visualisasi)
+print(harapan)
